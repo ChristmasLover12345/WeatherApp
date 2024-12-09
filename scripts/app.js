c1e18378fc85e02228bc8816771dec85d5dab7a5
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-09 12:04:21 -0800

    data fetches work

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+import { APIKEY } from "./key.js";
+
+let searchBar = document.getElementById('searchBar');
+
+
+// for testing
+// let lat = "52.3676";
+// let lon = "4.9041";
+// let userSearch = "Amsterdam"
+
+let lat = "";
+let lon = "";
+let userSearch = ""
+
+async function CurrentWeather(){
+
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
+    const data = await promise.json();
+    console.log(data)
+}
+
+async function forecastWeather(){
+
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
+    const data = await promise.json();
+    console.log(data)
+}
+
+async function getName(){
+
+    const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=1&appid=${APIKEY}`);
+    const data = await promise.json();
+    console.log(data)
+}
+
