c2a84101b538fcbbb68aa9b9c41c08a60a3857af
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-10 13:48:40 -0800

    set up place holders for main display

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,10 @@ import { APIKEY } from "./key.js";
 let myDropdown = document.getElementById('myDropdown');
 let dropdownBtn = document.getElementById('dropdownBtn');
 
+let day = document.getElementById('day')
+let date = document.getElementById('date')
+let currentTime = document.getElementById('currentTime')
+
 let searchBar = document.getElementById('searchBar');
 
 
@@ -11,6 +15,9 @@ let searchBar = document.getElementById('searchBar');
 // let lon = "4.9041";
 // let userSearch = "Amsterdam"
 
+let timeSinceEpoch = 0;
+
+// data fetches
 let lat = "";
 let lon = "";
 let userSearch = ""
@@ -19,7 +26,7 @@ async function CurrentWeather(){
 
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
     const data = await promise.json();
-    console.log(data)
+    console.log(data.dt)
 }
 
 async function forecastWeather(){
@@ -36,6 +43,8 @@ async function getName(){
     console.log(data)
 }
 
+
+// dropdown
 function dropdown(){
     myDropdown.classList.toggle("show")
 }
@@ -43,6 +52,40 @@ function dropdown(){
 dropdownBtn.addEventListener('click', function(){
     dropdown()
 })
+
+//Date and Time
+
+
+
+function dateNTime(){
+
+let startDay = new Date('January 2, 2024 00:00:00');
+let getDay = startDay.getDay()
+let currentDay = ["Sunday, ", "Monday, ", "Tuesday, ", "Wednesday, ", "Thursday, ", "Friday, ", "Saturday, "]
+
+day.innerText = currentDay[getDay]
+
+// I used Chat GPT to help me translate the time from seconds to the 12 hour, I wasnt able to find the answer trugh google.
+let millisecondsSinceEpoch = timeSinceEpoch * 1000;
+let date2 = new Date(millisecondsSinceEpoch);
+let timeRn = date2.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+});
+
+
+currentTime.innerText = timeRn
+
+}
+
+
+
+dateNTime()
+
+
+
+
 
 
 
