b991ee84cbd09b4d7019c73be2ed7c5e3d609ca6
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-10 15:20:43 -0800

    startde on backend

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,36 @@ import { APIKEY } from "./key.js";
 let myDropdown = document.getElementById('myDropdown');
 let dropdownBtn = document.getElementById('dropdownBtn');
 
-let day = document.getElementById('day')
-let date = document.getElementById('date')
-let currentTime = document.getElementById('currentTime')
+let day = document.getElementById('day');
+let date = document.getElementById('date');
+let currentTime = document.getElementById('currentTime');
 
 let searchBar = document.getElementById('searchBar');
 
+let currentIcon = document.getElementById('currentIcon');
+let maxTemp = document.getElementById('maxTemp');
+let minTemp = document.getElementById('minTemp');
+
+let forecast1Day = document.getElementById('forecast1Day');
+let forecastImg1 = document.getElementById('forecastImg1');
+let forcastMax1 = document.getElementById('forcastMax1');
+let forcastMin1 = document.getElementById('forcastMin1');
+let forecast2Day = document.getElementById('forecast2Day');
+let forecastImg2 = document.getElementById('forecastImg2');
+let forcastMax2 = document.getElementById('forcastMax2');
+let forcastMin2 = document.getElementById('forcastMin2');
+let forecast3Day = document.getElementById('forecast3Day');
+let forecastImg3 = document.getElementById('forecastImg3');
+let forcastMax3 = document.getElementById('forcastMax3');
+let forcastMin3 = document.getElementById('forcastMin3');
+let forecast4Day = document.getElementById('forecast4Day');
+let forecastImg4 = document.getElementById('forecastImg4');
+let forcastMax4 = document.getElementById('forcastMax4');
+let forcastMin4 = document.getElementById('forcastMin4');
+let forecast5Day = document.getElementById('forecast5Day');
+let forecastImg5 = document.getElementById('forecastImg5');
+let forcastMax5 = document.getElementById('forcastMax5');
+let forcastMin5 = document.getElementById('forcastMin5');
 
 // for testing
 // let lat = "52.3676";
@@ -18,8 +42,8 @@ let searchBar = document.getElementById('searchBar');
 let timeSinceEpoch = 0;
 
 // data fetches
-let lat = "";
-let lon = "";
+let lat = "52.3676";
+let lon = "4.9041";
 let userSearch = ""
 
 async function CurrentWeather(){
@@ -57,10 +81,10 @@ dropdownBtn.addEventListener('click', function(){
 
 
 
-function dateNTime(){
+function currentInfo(){
 // FOR SOME REASON IT CANT READ DATA.DT REMEMBER TO ASK A TEACEHR FOR HELP LATER
 CurrentWeather().then( data => {
-timeSinceEpoch = data.dt 
+timeSinceEpoch = data.dt })
 
 let startDay = new Date('January 2, 2024 00:00:00');
 let getDay = startDay.getDay()
@@ -80,11 +104,11 @@ let timeRn = date2.toLocaleTimeString('en-US', {
 
 currentTime.innerText = timeRn
 
-})}
+}
 
 
 
-// dateNTime()
+
 
 
 
